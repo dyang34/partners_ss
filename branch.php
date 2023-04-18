fcb26b26343057fd6c1d67c31c9890abaed703f1
dyang34 <dyang34@naver.com>
2023-04-18 15:01:49 +0900

    해외여행, 국내여행, 장기여행 순으로 서비스 여부 체크.

--- branch.php
+++ branch.php
@@ -9,6 +9,19 @@ if (!LoginManager::isUserLogined()) {
     //JsUtil::alertReplace("비정상적인 접근입니다."."/");
     JsUtil::replace("/");
 } else {
-    JsUtil::replace("/service/contract/register.php");
+
+    $trip_1_company_show = count(LoginManager::getUserLoginInfo('arr_trip_1_company'));
+    $trip_2_company_show = count(LoginManager::getUserLoginInfo('arr_trip_2_company'));
+    $trip_3_company_show = count(LoginManager::getUserLoginInfo('arr_trip_3_company'));
+
+    if (!empty($trip_2_company_show)) {
+        JsUtil::replace("/service/contract/register.php?trip_type=2");
+    } else if (!empty($trip_1_company_show)) {
+        JsUtil::replace("/service/contract/register.php?trip_type=1");
+    } else if (!empty($trip_3_company_show)) {
+        JsUtil::replace("/service/contract/register_long.php?trip_type=3");
+    } else {
+        JsUtil::replace("/service/calc/calc_list.php");
+    }
 }
 ?>
