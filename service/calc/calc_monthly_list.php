6cd275d8d862fcfe17f96b120df3e9e6a153e402
dyang34 <dyang34@naver.com>
2023-06-16 16:43:30 +0900

    담당자별 검색 강화.

--- service/calc/calc_monthly_list.php
+++ service/calc/calc_monthly_list.php
@@ -19,7 +19,17 @@ $_year_from = RequestUtil::getParam("_year_from", date("Y", strtotime("-1 month"
 $_year_to = RequestUtil::getParam("_year_to", date("Y"));
 $_month_from = RequestUtil::getParam("_month_from", date("m", strtotime("-1 month")));
 $_month_to = RequestUtil::getParam("_month_to", date("m"));
-$_manager_name = RequestUtil::getParam("_manager_name", "");
+$_manager_info = RequestUtil::getParam("_manager_info", "");
+
+if(!empty($_manager_info)) {
+    $arr_manager_info = explode('|', $_manager_info);
+
+    $_manager_name = $arr_manager_info[0];
+    $_manager_idx = $arr_manager_info[1];
+} else {
+    $_manager_name = "";
+    $_manager_idx = -1;
+}
 
 $date_from = $_year_from.sprintf("-%02d-01", $_month_from);
 if($_month_to=="12") {
@@ -33,7 +43,12 @@ $wq->addAndString("member_no","=",$__CONFIG_MEMBER_NO);
 $wq->addAndString("change_type","=","1");
 $wq->addAndStringBind("a.regdate", ">=", $date_from, "unix_timestamp('?')");
 $wq->addAndStringBind("a.regdate", "<", $date_to, "unix_timestamp('?')");
-$wq->addAndString("manager_name","=",$_manager_name);
+
+if($_manager_idx==0) {
+    $wq->addAndString2("ifnull(manager_idx, 0)","=","0");
+} else {
+    $wq->addAndString("manager_name","=",$_manager_name);
+}
 
 $wq_cancel = new WhereQuery(true, true);
 $wq_cancel->addAndString("member_no","=",$__CONFIG_MEMBER_NO);
